6b4e27ac154eaa0e99c12458b01c3f43613c820d
Lucia <202214497@alu.comillas.edu>
2024-10-01 14:40:00 +0200

    version1

---
+++ City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class City
+    {
+        private PoliceStation policeStation;
+        private List<Taxi> taxiLicenses;
+
+        void RegisterTaxiLicense(Taxi taxi)
+        {
+            taxiLicenses.Add(taxi);
+        }
+
+        void RemoveTaxiLicense(Taxi taxi)
+        {
+            taxiLicenses.Remove(taxi);
+        }
+
+    }
+}
---
+++ PoliceStation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class PoliceStation
+    {
+        private List<PoliceCar> policeCars;
+        private bool alert;
+
+        public void RegisterCar(PoliceCar policeCar)
+        {
+            policeCars.Add(policeCar);
+        }
+
+        public void NotifyPlate(string vehiclePlate)
+        {
+            for (int i = 0; i < policeCars.Count; i++)
+            {
+                PoliceCar policeCar = policeCars[i];
+                if (policeCar.IsPatrolling())
+                {
+                    Console.WriteLine("Police Car Notified");
+                    bool stationNotified = true;
+                    policeCar.StartChasing(vehiclePlate, stationNotified);
+                }
+            }
+        }
+
+        public void ActivateAlert(string vehiclePlate)
+        { 
+            if (!alert)
+            {  
+                alert = true;
+                NotifyPlate(vehiclePlate);
+            }
+        }
+
+        public void DeActivateAlert()
+        { 
+            alert = false; 
+        }
+    }
+}
